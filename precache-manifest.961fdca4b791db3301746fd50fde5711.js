10c731dac2469eafaa956815d4e330d67aba52a9
Code Sandbox <hello@codesandbox.io>
2021-06-21 18:42:29 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.961fdca4b791db3301746fd50fde5711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa7026bf754b384affc70368e8b9c83",
+    "url": "https://chrillep.github.io/csb-ojier/index.html"
+  },
+  {
+    "revision": "ad26b0c5a8bd647ab8bf",
+    "url": "https://chrillep.github.io/csb-ojier/static/css/main.189dde6d.chunk.css"
+  },
+  {
+    "revision": "2401c11c07d83766fb44",
+    "url": "https://chrillep.github.io/csb-ojier/static/js/2.09efac32.chunk.js"
+  },
+  {
+    "revision": "ad26b0c5a8bd647ab8bf",
+    "url": "https://chrillep.github.io/csb-ojier/static/js/main.ea6a82f7.chunk.js"
+  },
+  {
+    "revision": "d9ae846f4c180ba74281",
+    "url": "https://chrillep.github.io/csb-ojier/static/js/runtime~main.db95a76c.js"
+  }
+]);
